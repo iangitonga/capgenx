cc8f4e98932a2359fead152e75f81801d119ee8a
iangitonga <iangitonga.igg@gmail.com>
2023-03-04 15:03:58 +0300

    Updated configure script to not unzip default dev model

--- configure.py
+++ configure.py
@@ -5,15 +5,6 @@ import subprocess
 BASE_PATH = os.path.abspath(os.getcwd())
 
 
-# Unzip application models. -d temp_for_zip_extract
-# TODO: Should be done by application itself.
-
-models_path = os.path.join(BASE_PATH, "bin", "assets", "models")
-tiny_model_path = os.path.join(models_path, "tiny.zip")
-target_model_path = os.path.join(models_path, "tiny")
-subprocess.run([f"unzip {tiny_model_path} -d {target_model_path}"], shell=True, check=True)
-
-
 # Libtorch download binaries.
 LIBTORCH_CPU_DOWNLOAD_URL = "https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip"
 LIBTORCH_DOWNLOAD_PATH = os.path.join(BASE_PATH, "third_party")
